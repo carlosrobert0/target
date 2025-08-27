56d46b7c2323b2226a5e019886de16ef443b23bc
carlosrobert0 <carlos.reis.33@hotmail.com>
2025-08-26 21:44:35 -0300

    feat: create a SQLite database connection and create the targets and transactions tables

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,18 @@
 import { Slot } from 'expo-router'
-import { StatusBar, View } from 'react-native'
+import { View } from 'react-native'
 import '../../global.css'
+import { migrate } from '@/database/migrate'
+import { SQLiteProvider } from 'expo-sqlite'
+import { Suspense } from 'react'
+
 import {
   Inter_400Regular,
   Inter_500Medium,
   Inter_700Bold,
   useFonts,
 } from '@expo-google-fonts/inter'
+
+import { Loading } from '@/components/Loading'
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
@@ -16,12 +22,16 @@ export default function RootLayout() {
   })
 
   if (!loaded && !error) {
-    return null
+    return <Loading />
   }
 
   return (
-    <View className="flex-1 justify-center items-center">
-      <Slot />
-    </View>
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider onInit={migrate} databaseName="cofrin.db" useSuspense>
+        <View className="flex-1 justify-center items-center">
+          <Slot />
+        </View>
+      </SQLiteProvider>
+    </Suspense>
   )
 }
